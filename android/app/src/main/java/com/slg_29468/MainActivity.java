91796125d1295ba7d568f760db48d1935e0ed16b
Crowdbotics <team@crowdbotics.com>
2021-08-04 18:57:24 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/slg_29468/MainActivity.java
@@ -0,0 +1,15 @@
+package com.slg_29468;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "slg_29468";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "slg_29468Identifier",
+  url: "https://slg_29468Identifier.botics.co",
+  api: "https://slg_29468Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
